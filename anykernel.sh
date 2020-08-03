9089ab8b78d7380b33078e86800f4e6028f64d65
MoveAngel <moveangel@minaproject.tech>
2020-08-03 22:31:51 +0700

    anykernel: derp
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=M3 Kernel By MoveAngel Telegram:@MoveAngel Channel: @M3Kernel
+kernel.string=M3 Kernel By MoveAngel Telegram: @MoveAngel Channel: @M3Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=0
