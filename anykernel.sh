f5af98d8989505b425984d20ba06bf3368a7d502
MoveAngel <moveangel@minaproject.tech>
2020-08-03 22:29:52 +0700

    anykernel: bringup m3 kernel
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Mina-Kernel By MoveAngel Telegram: @MoveAngel Channel: @MinaKernel
+kernel.string=M3 Kernel By MoveAngel Telegram:@MoveAngel Channel: @M3Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=0
